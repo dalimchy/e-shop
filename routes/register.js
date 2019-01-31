79eb0d8b9e86c8df95068e8aa1d752c7b1bbcae7
dalimchy <dalimchyjony@gmail.com>
2019-01-31 17:58:22 +0600

    upadte profile image

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './public/admin_assets/images/users/')
+    },
+    filename: (req, file, cb) => {
+      if(file.fieldname == 'profile_pic'){
+        var user_name = req.body.name;
+        var fileType = file.originalname.split('.');
+        cb(null, user_name.split(' ').join('_')+ '@' + Date.now()+'.'+fileType[1]);
+      }
+      
+    }
+});
+var upload = multer({storage: storage}).single('profile_pic');
+
 
 var {insertUsers} = require('./../utils/users');
 
@@ -18,34 +34,40 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/newUser', function(req, res) {
-  if(req.session.login){
-    res.redirect('/dashboard');
-  }else{
-    var data = {
-      user_name : req.body.name,
-      user_email : req.body.email,
-      user_phone : req.body.phone,
-      user_password : req.body.password,
-      user_img : 'avatar-1.jpg'
-    }
-    insertUsers(data, (response)=>{
-      if(response.msg == 'success'){
-        req.session.success = true;
-        req.session.login = true;
-        req.session.user_id = response.data.user_id;
-        req.session.user_name = response.data.user_name;
-        req.session.user_email = response.data.user_email;
-        req.session.user_img = response.data.user_img;
+  upload(req,res, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      if(req.session.login){
         res.redirect('/dashboard');
       }else{
         var data = {
-          title : 'Register',
-          msg : response.msg
+          user_name : req.body.name,
+          user_email : req.body.email,
+          user_phone : req.body.phone,
+          user_password : req.body.password,
+          user_img : res.req.file.filename
         }
-        res.render('pages/dashboard/register', data);
+        insertUsers(data, (response)=>{
+          if(response.msg == 'success'){
+            req.session.success = true;
+            req.session.login = true;
+            req.session.user_id = response.data.user_id;
+            req.session.user_name = response.data.user_name;
+            req.session.user_email = response.data.user_email;
+            req.session.user_img = response.data.user_img;
+            res.redirect('/dashboard');
+          }else{
+            var data = {
+              title : 'Register',
+              msg : response.msg
+            }
+            res.render('pages/dashboard/register', data);
+          }
+        });
       }
-    });
-  }
+    }
+  });
 });
 
 module.exports = router;
